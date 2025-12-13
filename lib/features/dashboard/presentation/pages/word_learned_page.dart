cb8c3073490af53bb3196365b110b7d77173c99b
TranDucNam17 <superouvorlurd@gmail.com>
2025-12-13 07:32:37 +0700

    [Update]Update UI WordLearned screen

--- lib/features/dashboard/presentation/pages/word_learned_page.dart
+++ lib/features/dashboard/presentation/pages/word_learned_page.dart
@@ -80,24 +80,6 @@ class _WordLearnedScreenState extends State<WordLearnedScreen> {
           ),
         ),
       ),
-      // BottomNavigationBar
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) { /* TODO: Handle navigation */ },
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: primaryBlue,
-        unselectedItemColor: Colors.grey[600],
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        backgroundColor: Colors.white,
-        elevation: 8.0,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.book_outlined), label: 'Vocab'),
-          BottomNavigationBarItem(icon: Icon(Icons.spellcheck_outlined), label: 'Grammar'),
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Profile'),
-        ],
-      ),
     );
   }
 }
