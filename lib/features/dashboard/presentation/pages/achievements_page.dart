bf003271f902cf37997a47b70c7e4a9c88fda7ba
TranDucNam17 <superouvorlurd@gmail.com>
2025-12-13 07:29:44 +0700

    [Update]Update UI Achievements screen

--- lib/features/dashboard/presentation/pages/achievements_page.dart
+++ lib/features/dashboard/presentation/pages/achievements_page.dart
@@ -52,29 +52,6 @@ class AchievementsPage extends StatelessWidget {
         ),
       ),
       // BottomNavigationBar
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // Tab "Home" được chọn
-        onTap: (index) {
-          /* TODO: Handle navigation */
-        },
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: primaryBlue,
-        unselectedItemColor: Colors.grey[600],
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        backgroundColor: Colors.white,
-        elevation: 8.0,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_filled), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.book_outlined), label: 'Vocab'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.spellcheck), label: 'Grammar'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline), label: 'Profile'),
-        ],
-      ),
     );
   }
 
