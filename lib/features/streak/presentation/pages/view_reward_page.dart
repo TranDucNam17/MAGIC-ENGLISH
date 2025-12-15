bbc2baa723ad80bbff31ac13f36bec9b48f68940
TranDucNam17 <superouvorlurd@gmail.com>
2025-12-15 16:31:40 +0700

    [Fix]Fix UI ViewReward screen

--- lib/features/streak/presentation/pages/view_reward_page.dart
+++ lib/features/streak/presentation/pages/view_reward_page.dart
@@ -63,26 +63,6 @@ class _ViewRewardsScreenState extends State<ViewRewardsScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: (index) {},
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: primaryBlue,
-        unselectedItemColor: Colors.grey[600],
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        backgroundColor: Colors.white,
-        elevation: 8.0,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.book_outlined), label: 'Vocab'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.spellcheck_outlined), label: 'Grammar'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline), label: 'Profile'),
-        ],
-      ),
     );
   }
 }
