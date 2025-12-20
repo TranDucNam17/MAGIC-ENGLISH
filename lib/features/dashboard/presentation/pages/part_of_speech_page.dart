a74dacb9e09785038491eba714f985db0e76bee0
TranDucNam17 <superouvorlurd@gmail.com>
2025-12-20 07:26:45 +0700

    [Update]Update UI PartOfSpeech

--- lib/features/dashboard/presentation/pages/part_of_speech_page.dart
+++ lib/features/dashboard/presentation/pages/part_of_speech_page.dart
@@ -8,11 +8,9 @@ class PartOfSpeechScreen extends StatefulWidget {
 }
 
 class _PartOfSpeechScreenState extends State<PartOfSpeechScreen> {
-  final int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    const Color primaryBlue = Color(0xFF0D47A1);
     const Color lightBlueBackground = Color(0xFFF7F9FC);
     const Color darkText = Color(0xFF1A252F);
 
